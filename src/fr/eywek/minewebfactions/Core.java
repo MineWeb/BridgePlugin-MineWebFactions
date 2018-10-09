aab6f0e6503c1ad415727aa9e5879ff8d7aaeff7
Kuro <8905889+KuroSenseii@users.noreply.github.com>
2018-10-09 16:23:59 -0400

    add faction 2.14
    add faction 2.14

--- src/fr/eywek/minewebfactions/Core.java
+++ src/fr/eywek/minewebfactions/Core.java
@@ -2,6 +2,7 @@ package fr.eywek.minewebfactions;
 
 import fr.eywek.minewebfactions.methods.GetFactions2_13;
 //import fr.eywek.minewebfactions.methods.GetFactions2_3;
+import fr.eywek.minewebfactions.methods.GetFactions2_14;
 import fr.eywek.minewebfactions.methods.GetFactions2_8;
 import fr.eywek.minewebfactions.methods.GetFactionsUUID;
 import fr.vmarchaud.mineweb.bukkit.BukkitCore;
@@ -13,12 +14,14 @@ public class Core extends JavaPlugin {
     @Override
     public void onEnable() {
         String version = Bukkit.getPluginManager().getPlugin("Factions").getDescription().getVersion();
-        if (version.startsWith("2.13"))
+        if (version.startsWith("2.14"))
+            BukkitCore.get().getMethods().put("GET_FACTIONS", new GetFactions2_14());
+        else if (version.startsWith("2.13"))
             BukkitCore.get().getMethods().put("GET_FACTIONS", new GetFactions2_13());
         else if (version.startsWith("2.8"))
             BukkitCore.get().getMethods().put("GET_FACTIONS", new GetFactions2_8());
-        //else if (version.startsWith("2.3") || version.startsWith("2.5"))
-        //    BukkitCore.get().getMethods().put("GET_FACTIONS", new GetFactions2_3());
+            //else if (version.startsWith("2.3") || version.startsWith("2.5"))
+            //    BukkitCore.get().getMethods().put("GET_FACTIONS", new GetFactions2_3());
         else if (version.startsWith("1.6"))
             BukkitCore.get().getMethods().put("GET_FACTIONS", new GetFactionsUUID());
     }
