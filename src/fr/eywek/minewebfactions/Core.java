1cb0540f013f7dc57bf190e5f795627e98dcdbbf
Kuro <maximelinteau123@gmail.com>
2019-01-22 22:34:34 -0500

    Add Faction 3.0.5

--- src/fr/eywek/minewebfactions/Core.java
+++ src/fr/eywek/minewebfactions/Core.java
@@ -3,6 +3,7 @@ package fr.eywek.minewebfactions;
 import fr.eywek.minewebfactions.methods.GetFactions2_13;
 //import fr.eywek.minewebfactions.methods.GetFactions2_3;
 import fr.eywek.minewebfactions.methods.GetFactions2_14;
+import fr.eywek.minewebfactions.methods.GetFactions3_0_5;
 import fr.eywek.minewebfactions.methods.GetFactions2_8;
 import fr.eywek.minewebfactions.methods.GetFactionsUUID;
 import fr.vmarchaud.mineweb.bukkit.BukkitCore;
@@ -14,7 +15,9 @@ public class Core extends JavaPlugin {
     @Override
     public void onEnable() {
         String version = Bukkit.getPluginManager().getPlugin("Factions").getDescription().getVersion();
-        if (version.startsWith("2.14"))
+        if (version.startsWith("3.0"))
+            BukkitCore.get().getMethods().put("GET_FACTIONS", new GetFactions3_0_5());
+        else if (version.startsWith("2.14"))
             BukkitCore.get().getMethods().put("GET_FACTIONS", new GetFactions2_14());
         else if (version.startsWith("2.13"))
             BukkitCore.get().getMethods().put("GET_FACTIONS", new GetFactions2_13());
---
+++ src/fr/eywek/minewebfactions/methods/GetFactions3_0_5.java
@@ -0,0 +1,56 @@
+package fr.eywek.minewebfactions.methods;
+
+import com.massivecraft.factions.entity.Faction;
+import com.massivecraft.factions.entity.FactionColl;
+import com.massivecraft.factions.entity.MPlayer;
+import fr.vmarchaud.mineweb.common.ICore;
+import fr.vmarchaud.mineweb.common.IMethod;
+import fr.vmarchaud.mineweb.common.MethodHandler;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.Objects;
+
+@MethodHandler
+public class GetFactions3_0_5 implements IMethod {
+
+    @Override
+    public Object execute(ICore instance, Object... inputs) {
+        @SuppressWarnings("rawtypes")
+        ArrayList<Hashtable> factions = new ArrayList<>();
+        for (Faction faction : FactionColl.get().getAll()) {
+            if (Objects.equals(faction.getName(), FactionColl.get().getNone().getName()) ||
+                    Objects.equals(faction.getName(), FactionColl.get().getSafezone().getName()) ||
+                    Objects.equals(faction.getName(), FactionColl.get().getWarzone().getName()))
+                continue;
+            // Basic
+            Hashtable<String, Object> f = new Hashtable<>();
+            f.put("id", faction.getId());
+            f.put("name", faction.getName());
+            f.put("description", faction.getDescriptionDesc());
+            f.put("claims_count", faction.getLandCount());
+
+            // Power
+            Hashtable<String, Double> power = new Hashtable<>();
+            power.put("max", faction.getPowerMax());
+            power.put("current", faction.getPower());
+            f.put("power", power);
+            try {
+                f.put("leader", faction.getLeader().getName());
+            }catch(Exception ex) {f.put("leader", "none");}
+
+
+            // Players
+            ArrayList<String> players = new ArrayList<>();
+            for (MPlayer mplayer : faction.getMPlayers()) {
+                players.add(mplayer.getName());
+            }
+            f.put("players", players);
+
+            // Add
+            factions.add(f);
+        }
+        return factions;
+    }
+
+}
