8043d21079c861a294469ef5d374b3697c7b8bbc
Kuro <maximelinteau123@gmail.com>
2019-05-01 13:56:25 -0400

    Add Faction 3 (#3)

--- src/fr/eywek/minewebfactions/Core.java
+++ src/fr/eywek/minewebfactions/Core.java
@@ -3,6 +3,7 @@ package fr.eywek.minewebfactions;
 import fr.eywek.minewebfactions.methods.GetFactions2_13;
 //import fr.eywek.minewebfactions.methods.GetFactions2_3;
 import fr.eywek.minewebfactions.methods.GetFactions2_14;
+import fr.eywek.minewebfactions.methods.GetFactions3;
 import fr.eywek.minewebfactions.methods.GetFactions2_8;
 import fr.eywek.minewebfactions.methods.GetFactionsUUID;
 import fr.vmarchaud.mineweb.bukkit.BukkitCore;
@@ -14,7 +15,9 @@ public class Core extends JavaPlugin {
     @Override
     public void onEnable() {
         String version = Bukkit.getPluginManager().getPlugin("Factions").getDescription().getVersion();
-        if (version.startsWith("2.14"))
+        if (version.startsWith("3"))
+            BukkitCore.get().getMethods().put("GET_FACTIONS", new GetFactions3());
+        else if (version.startsWith("2.14"))
             BukkitCore.get().getMethods().put("GET_FACTIONS", new GetFactions2_14());
         else if (version.startsWith("2.13"))
             BukkitCore.get().getMethods().put("GET_FACTIONS", new GetFactions2_13());
